e57c3d05ebf7d642d74cdd5965d8659dbe54b499
OntongGhebi <ghebiarmando@gmail.com>
2025-02-09 21:52:13 +0700

    Progress 10%

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { ThemeProvider } from "./contents/theme.contents";
+import Layout from "./routes/layout";
+import DashboardPage from "./routes/dashboard/page"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <DashboardPage />,
+        },
+        {
+          path: 'analytics',
+          element: <h1 className="title">Analytics</h1>,
+        },
+        {
+          path: 'reports',
+          element: <h1 className="title">Reports</h1>,
+        },
+        {
+          path: 'customers',
+          element: <h1 className="title">Customers</h1>,
+        },
+        {
+          path: 'new-customer',
+          element: <h1 className="title">New Customer</h1>,
+        },
+        {
+          path: 'verified-customers',
+          element: <h1 className="title">Verified Customers</h1>,
+        },
+        {
+          path: 'product',
+          element: <h1 className="title">Product</h1>,
+        },
+        {
+          path: 'new-product',
+          element: <h1 className="title">New Product</h1>,
+        },
+        {
+          path: 'inventory',
+          element: <h1 className="title">Inventory</h1>,
+        },
+        {
+          path: 'settings',
+          element: <h1 className="title">Settings</h1>,
+        },
+      ],
+    },
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider storageKey="theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
 
---
+++ src/routes/layout.jsx
@@ -0,0 +1,7 @@
+const Layout = () => {
+    return <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-900">
+        
+    </div>;
+    };
+
+export default Layout;
